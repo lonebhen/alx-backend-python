3dbc6791e85a76bea92df9948a9605cb5f63f4f4
lonebhen <ben.angmortey@gmail.com>
2023-02-04 05:10:57 +0000

    concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""Annotations"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """ returns a concatenated string"""
+    return str1 + str2
+
+
+# str1 = "egg"
+# str2 = "shell"
+
+# print(concat(str1, str2))
+# print(concat.__annotations__)

d0e6b57b339620850bb47abc6f724f127ec51bb2
lonebhen <ben.angmortey@gmail.com>
2023-02-04 05:42:25 +0000

    7

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""Annotations"""
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """convert to tuple"""
+
+    return (k, float(v**2))

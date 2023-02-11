7e926b16c7958a70603c1eeb1c8369306b5bfc63
lonebhen <ben.angmortey@gmail.com>
2023-02-11 00:54:31 +0000

    change function call

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""Concurrent coroutines"""
+
+import asyncio
+from typing import List
+
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """function returns list"""
+
+    tmp = [asyncio.create_task(task_wait_random(max_delay)) for i in range(n)]
+    waiting_time = [await task for task in asyncio.as_completed(tmp)]
+    return waiting_time
